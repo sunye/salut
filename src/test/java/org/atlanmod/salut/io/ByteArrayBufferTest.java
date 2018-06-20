7b9c77fe949948eac0c2dd4a4349eb51ecfd50fd
Nitrum666 <superboos@hotmail.fr>
2018-06-20 18:47:01 +0200

    Ajout de commentaires dans la classe ByteArrayBufferTest

--- src/test/java/org/atlanmod/salut/io/ByteArrayBufferTest.java
+++ src/test/java/org/atlanmod/salut/io/ByteArrayBufferTest.java
@@ -6,13 +6,19 @@ import java.text.ParseException;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+/*
+Classe de test pour la classe ByteArrayBuffer
+Cette classe a pour but de faire l'ensemble des tests unitaires de ByteArrayBuffer
+ */
 class ByteArrayBufferTest {
 
     @Test
     void wrap() {
     }
 
+    /*
+        Test de la méthode ReadLabels
+     */
     @Test
     void testReadLabels() throws ParseException {
         byte[] bytes = {8, 109, 121, 100, 111, 109, 97, 105, 110, 3, 99, 111, 109, 0};
@@ -25,6 +31,9 @@ class ByteArrayBufferTest {
         assertTrue(labels.contains("com"));
     }
 
+    /*
+    Test de la méthode ReadLabels en provoquant une exception
+     */
     @Test
     void testReadLabelsException()  {
 
@@ -90,6 +99,9 @@ class ByteArrayBufferTest {
     void getUnsignedInteger() {
     }
 
+    /*
+    Test de la méthode wrap, on vérifie si le tableau retourné est bien égal au tableau retourné par la méthode .array()
+     */
     @Test
     void array() {
         byte[] name = {8, 109, 121, 100, 111, 109, 97, 105, 110, 3, 99, 111, 109, 0};
@@ -97,6 +109,9 @@ class ByteArrayBufferTest {
         assertEquals(name, bb.array());
     }
 
+    /*
+    Test de la méthode position, on vérifie si le buffer est bien initialement à la position 0
+     */
     @Test
     void position() {
         byte[] name = {8, 109, 121, 100, 111, 109, 97, 105, 110, 3, 99, 111, 109, 0};
@@ -108,6 +123,9 @@ class ByteArrayBufferTest {
     void position1() {
     }
 
+    /*
+    Test de la méthode Get, on vérifie si l'élément retourné est bien celui inséré.
+     */
     @Test
     void get() {
         byte[] bytes = {5};
@@ -115,6 +133,9 @@ class ByteArrayBufferTest {
         assertEquals(5, ByteArrayBuffer.wrap(bytes).get());
     }
 
+    /*
+    Test de la méthode get, on vérifie que chaque élément est bien à la bonne position
+     */
     @Test
     void testGetAtPosition() {
         byte[] bytes = {5, 6, 7, 8, 9};
@@ -124,6 +145,9 @@ class ByteArrayBufferTest {
         }
     }
 
+    /*
+    Test de la méthode toString(), on vérifie que la chaine de caractère retournée est celle attendue
+     */
     @Test
     void testToString(){
         byte[] bytes = {5, 6, 7, 8, 9};
@@ -131,6 +155,9 @@ class ByteArrayBufferTest {
         assertEquals("java.nio.HeapByteBuffer[pos=0 lim=5 cap=5]", bb.toString());
     }
 
+    /*
+    Test de readLabels, on vérifie que le cas d'erreur se produise bien
+     */
     @Test
     void testCheckOut()  {
         assertAll(
