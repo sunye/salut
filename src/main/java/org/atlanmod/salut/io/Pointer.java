7ffab7afa84c072828a6260c90b7707836014eeb
Gerson Sunyé <gerson.sunye@gmail.com>
2018-06-28 09:42:58 +0200

    Add comments

--- src/main/java/org/atlanmod/salut/io/Pointer.java
+++ src/main/java/org/atlanmod/salut/io/Pointer.java
@@ -1,5 +1,15 @@
 package org.atlanmod.salut.io;
 
+/**
+ * A label pointer is an unsigned 16-bit value, where the top two bits of 11 indicate the pointer format,
+ * and the 14 other bits represent a (previous) position containing one or more labels (in the same dns message).
+ *
+ * For instance, if a message contains several times the labels "salut.atlanmod.org", only the first occurrence
+ * will be written to the message, the others will use pointers to this first occurrence.
+ *
+ * While inb theory the position may be between 0 and 2^14 (16,384), it is limited between the position the label
+ * pointer was read and the mdns messages maximum size.
+ */
 public class Pointer extends UnsignedShort {
     private static final short POINTER_MASK = 0x3FFF;
 
